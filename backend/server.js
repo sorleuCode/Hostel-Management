98da067d6b88847599dd988b18fe9b0e1b4a17c7
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2024-06-13 11:35:29 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 })
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://hostel-management-67gp17zpv-sorleus-projects.vercel.app"],
     credentials: true,
     optionsSuccessStatus: 200,
     methods: "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS"
